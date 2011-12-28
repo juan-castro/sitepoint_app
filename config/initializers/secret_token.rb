6abf2286aae7abf88036104744e7ae6757dc3d2d
Juan Castro <j.castro@tukaiz.com>
2011-12-28 12:26:19 -0600

    Initial installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SitepointApp::Application.config.secret_token = '7c91a985f908f4624ef73d1fce426764242fee69be7c00bba41fe0e49ecedf381fe896027eaa2f22437e2459a9468a6cdb3f353e4411f5f8dbf7d95d1dabe33c'
